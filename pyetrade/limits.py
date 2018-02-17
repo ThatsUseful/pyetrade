79cf8e4315a988eda3d975600b179bdb0e39264d
scottrutherford <scott@thatsuseful.com>
2018-02-16 16:03:20 -0800

    limits

--- pyetrade/limits.py
+++ pyetrade/limits.py
@@ -1,6 +1,6 @@
 '''Limits - ETrade Limits API'''
 import logging
-from requests_oauthlib import OAuth1Session
+from requests
 
 # Set up logging
 logger = logging.getLogger(__name__)
@@ -8,32 +8,19 @@ logger = logging.getLogger(__name__)
 
 class ETradeLimits(object):
   '''ETradeLimits'''
-  def __init__(self, client_key, client_secret,
-               resource_owner_key, resource_owner_secret):
+  def __init__(self, client_key, resource_owner_key):
       '''__init__(client_key, client_secret)
          param: client_key
          type: str
          description: etrade client key
-         param: client_secret
-         type: str
-         description: etrade client secret
          param: resource_owner_key
          type: str
-         description: OAuth authentication token key
-         param: resource_owner_secret
-         type: str
-         description: OAuth authentication token secret'''
+         description: OAuth authentication token key'''
       self.client_key = client_key
-      self.client_secret = client_secret
       self.resource_owner_key = resource_owner_key
-      self.resource_owner_secret = resource_owner_secret
       self.base_url_prod = r'https://etws.etrade.com'
       self.base_url_dev = r'https://etwssandbox.etrade.com'
-      self.session = OAuth1Session(self.client_key,
-                                   self.client_secret,
-                                   self.resource_owner_key,
-                                   self.resource_owner_secret,
-                                   signature_type='AUTH_HEADER')
+
 
 
   def get_limits(self, module, dev=True, resp_format='json'):
@@ -57,8 +44,12 @@ class ETradeLimits(object):
 
 
       #add detail flag to url
-      payload = {'module': module}
-      req = self.session.get(api_url, params=payload)
+      payload = {
+        'module': module,
+        'oauth_token': self.resource_owner_key,
+        'oauth_consumer_key': self.client_key
+      }
+      req = requests.get(api_url, params=payload)
       req.raise_for_status()
       logger.debug(req.text)
 
